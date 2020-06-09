9f613e5af177d5799bcefa1c173383f534087dbe
WMWjun <wmwspecter@163.com>
2020-06-10 01:16:22 +0800

    完美运动框架

--- 04-运动/运动中级/完美运动框架/move.js
+++ 04-运动/运动中级/完美运动框架/move.js
@@ -8,33 +8,34 @@ function getStyle(obj, name) {
     }
 }
 
-function startMove(obj, attr, target, fnEnd) {
+function startMove(obj, json, fnEnd) {
     clearInterval(obj.timer);
-
     obj.timer = setInterval(function () {
-        var cur = 0;
+        for (var attr in json) {
+            var cur = 0;
 
-        if (attr == 'opacity') {
-            cur = Math.round(parseFloat(getStyle(obj, 'opacity')) * 100);
-        } else {
-            cur = parseInt(getStyle(obj, attr));
-        }
-
-        var speed = 0;
-        speed = (target - cur) / 6;
-        speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-
-        if (cur == target) {
-            clearInterval(obj.timer);
-            if (fnEnd) {
-                fnEnd();
-            }
-        } else {
             if (attr == 'opacity') {
-                obj.style.filter = 'alpha(opacity = ' + (cur + speed) + ')';
-                obj.style.opacity = (cur + speed) / 100;
+                cur = Math.round(parseFloat(getStyle(obj, 'opacity')) * 100);
             } else {
-                obj.style[attr] = cur + speed + 'px';
+                cur = parseInt(getStyle(obj, attr));
+            }
+
+            var speed = 0;
+            speed = (json[attr] - cur) / 6;
+            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+
+            if (cur == json[attr]) {
+                clearInterval(obj.timer);
+                if (fnEnd) {
+                    fnEnd();
+                }
+            } else {
+                if (attr == 'opacity') {
+                    obj.style.filter = 'alpha(opacity = ' + (cur + speed) + ')';
+                    obj.style.opacity = (cur + speed) / 100;
+                } else {
+                    obj.style[attr] = cur + speed + 'px';
+                }
             }
         }
     }, 30);
